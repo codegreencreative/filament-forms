4934148dbf5285e9101cf874f795b107dd89500c
Jason Beach <jason@codegreencreative.com>
2023-10-27 15:38:16 -0400

    Update provider with correct path

--- src/FormsServiceProvider.php
+++ src/FormsServiceProvider.php
@@ -25,16 +25,16 @@ class FormsServiceProvider extends PackageServiceProvider
     public function packageBooted(): void
     {
         FilamentAsset::register([
-            AlpineComponent::make('color-picker', __DIR__ . '/../dist-bak/components/color-picker.js'),
-            AlpineComponent::make('date-time-picker', __DIR__ . '/../dist-bak/components/date-time-picker.js'),
-            AlpineComponent::make('file-upload', __DIR__ . '/../dist-bak/components/file-upload.js'),
-            AlpineComponent::make('key-value', __DIR__ . '/../dist-bak/components/key-value.js'),
-            AlpineComponent::make('markdown-editor', __DIR__ . '/../dist-bak/components/markdown-editor.js'),
-            AlpineComponent::make('rich-editor', __DIR__ . '/../dist-bak/components/rich-editor.js'),
-            AlpineComponent::make('select', __DIR__ . '/../dist-bak/components/select.js'),
-            AlpineComponent::make('tags-input', __DIR__ . '/../dist-bak/components/tags-input.js'),
-            AlpineComponent::make('textarea', __DIR__ . '/../dist-bak/components/textarea.js'),
-            Css::make('forms', __DIR__ . '/../dist-bak/index.css'),
+            AlpineComponent::make('color-picker', __DIR__ . '/../dist/components/color-picker.js'),
+            AlpineComponent::make('date-time-picker', __DIR__ . '/../dist/components/date-time-picker.js'),
+            AlpineComponent::make('file-upload', __DIR__ . '/../dist/components/file-upload.js'),
+            AlpineComponent::make('key-value', __DIR__ . '/../dist/components/key-value.js'),
+            AlpineComponent::make('markdown-editor', __DIR__ . '/../dist/components/markdown-editor.js'),
+            AlpineComponent::make('rich-editor', __DIR__ . '/../dist/components/rich-editor.js'),
+            AlpineComponent::make('select', __DIR__ . '/../dist/components/select.js'),
+            AlpineComponent::make('tags-input', __DIR__ . '/../dist/components/tags-input.js'),
+            AlpineComponent::make('textarea', __DIR__ . '/../dist/components/textarea.js'),
+            Css::make('forms', __DIR__ . '/../dist/index.css'),
         ], 'filament/forms');
 
         if ($this->app->runningInConsole()) {
